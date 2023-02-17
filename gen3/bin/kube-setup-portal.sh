23004e9840597985d3cd924155f7203b7c75fe54
Luca Graglia <graglia01@gmail.com>
2023-02-17 16:38:02 -0600

    ternary operator for ENV variable in portal params

--- gen3/bin/kube-setup-portal.sh
+++ gen3/bin/kube-setup-portal.sh
@@ -54,4 +54,5 @@ fi
 dataUploadBucketName=$(gen3 secrets decode fence-config fence-config.yaml | yq -r .DATA_UPLOAD_BUCKET)
 gearbox_s3_bucket=$(gen3 secrets decode portal-config gitops.json | jq .s3_bucket)
 g3kubectl apply -f "${GEN3_HOME}/kube/services/portal/portal-service.yaml"
-gen3 roll portal GEN3_DATA_UPLOAD_BUCKET $dataUploadBucketName GEN3_GEARBOX_S3_BUCKET $gearbox_s3_bucket
+# gen3 roll portal GEN3_DATA_UPLOAD_BUCKET $dataUploadBucketName GEN3_GEARBOX_S3_BUCKET $gearbox_s3_bucket
+gen3 roll portal $([ -z ${dataUploadBucketName+x}" ] && echo "GEN3_DATA_UPLOAD_BUCKET $dataUploadBucketName " || echo "") $([ -z ${gearbox_s3_bucket+x}" ] && echo "GEN3_GEARBOX_S3_BUCKET $gearbox_s3_bucket " || echo "")
