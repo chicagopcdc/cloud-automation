43c8d8c3cc806f3264a2c5e50f2919f3bdb98663
Luca Graglia <graglia01@gmail.com>
2025-03-27 10:44:49 -0500

    BYPASS_IMPORTANT_QUESTIONS for GEARBOx in gearbox.env config

--- gen3/bin/kube-setup-gearbox.sh
+++ gen3/bin/kube-setup-gearbox.sh
@@ -53,6 +53,7 @@ S3_AWS_SECRET_ACCESS_KEY=$(jq -r .gearbox.gearbox_bucket_aws_access_key < "$GEN3
 
 ADMIN_LOGINS=gateway:$password
 ENABLE_PHI=$ENABLE_PHI
+BYPASS_IMPORTANT_QUESTIONS=True
 EOM
     # make it easy for nginx to get the Authorization header ...
     echo -n "gateway:$password" | base64 > "$secretsFolder/base64Authz.txt"
